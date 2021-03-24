311f62ccda31367a5daf2d92cbecf3ca27a60ec0
Urszula Suplicka <usuplicka@gmail.com>
2021-03-24 12:05:13 +0100

    Added isRequired to some props

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -26,11 +26,11 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
 
 TripSummary.propTypes = {
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  image: PropTypes.string,
-  name: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   intro: PropTypes.string,
-  cost: PropTypes.string,
-  days: PropTypes.number,
+  cost: PropTypes.string.isRequired,
+  days: PropTypes.number.isRequired,
   tags: PropTypes.array,
 };
 
--- src/components/features/TripSummary/TrpSummary.test.js
+++ src/components/features/TripSummary/TrpSummary.test.js
@@ -10,7 +10,7 @@ describe('Component TripSummary', () => {
   it('should render correct address', () => {
     const expectedId = 'abc';
     const expectedLink = '/trip/abc';
-    const component = shallow(<TripSummary id={expectedId} tags={[]} />); // hm, tez musialy zostac tagi, bo inaczej sie crashuje
+    const component = shallow(<TripSummary id={expectedId} image='image' name='name' cost='cost' days={0} tags={[]} />);
 
     expect(component.find('.link').prop('to')).toEqual(expectedLink);
   });
@@ -18,15 +18,29 @@ describe('Component TripSummary', () => {
   it('img has correct src and alt', () => {
     const expectedSrc = 'image';
     const expectedAlt = 'name';
-    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} tags={[]}/>); //dlaczego wyrzuca blad jak brak tagow??
+    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} cost='cost' days={0} tags={[]} />); //dlaczego wyrzuca blad jak brak tagow??
 
     expect(component.find('img').prop('src')).toEqual(expectedSrc);
     expect(component.find('img').prop('alt')).toEqual(expectedAlt);
   });
 
   it('should render proper name, cost and days', () => {
-    const component = shallow(<TripSummary name='name' cost='cost' days={0} tags={[]}/>);
+    const component = shallow(<TripSummary name='name' cost='cost' days={0} tags={[]} />);
     expect(component).toBeTruthy();
+  });
+
+  it('should render tags in correct order', () => {
+    const component = shallow(<TripSummary tags={['tag1', 'tag2', 'tag3']} image='image' name='name' cost='cost' days={0} />);
+
+    expect(component.find('.tags span').at(0).text()).toEqual('tag1');
+    expect(component.find('.tags span').at(1).text()).toEqual('tag2');
+    expect(component.find('.tags span').at(2).text()).toEqual('tag3');
+  });
+
+  it('should throw error without required tags', () => {
+    const expectedTags = [];
+    const component = shallow(<TripSummary tags={expectedTags} image='image' name='name' cost='cost' days={0} />);
+    expect(component.hasClass('tags')).toBe(false);
   });
 });
 
